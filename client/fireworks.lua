870d48eb89de83367cced43474844745c9d6a02f
TecnohShinonome <TecnohShinonome@users.noreply.github.com>
2025-08-14 22:37:06 +0900

    font

--- client/fireworks.lua
+++ client/fireworks.lua
@@ -37,7 +37,7 @@ local fireworkList = {
 
 local function DrawText3D(x, y, z, text)
     SetTextScale(0.35, 0.35)
-    SetTextFont(4)
+    SetTextFont(0)
     SetTextProportional(true)
     SetTextColour(255, 255, 255, 215)
     BeginTextCommandDisplayText('STRING')
